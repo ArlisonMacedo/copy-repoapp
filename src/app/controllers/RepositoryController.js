6050914dde3c84f4ff78e1a5ee0ba058169e4180
ArlisonMacedo <arlisonmacedo526@gmail.com>
2020-05-24 18:44:30 -0300

    Controller

---
+++ src/app/controllers/RepositoryController.js
@@ -0,0 +1,46 @@
+import Repository from '../models/Repository';
+import { Op } from 'sequelize';
+import * as Yup from 'yup';
+
+class RepositoryController {
+  async index(req, res) {
+    // const { tag = '' } = req.params;
+    const repository = await Repository.findAll({
+      where: {
+        tags: [([Op.iLike] = '%api')],
+      },
+    });
+
+    return res.json({
+      repository,
+    });
+  }
+
+  //Create Repository
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      title: Yup.string().required(),
+      link: Yup.string().required(),
+      description: Yup.string().required(),
+      tags: Yup.array().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ erro: 'Invalid data' });
+    }
+
+    const { id, title, link, description, tags } = await Repository.create(
+      req.body
+    );
+
+    return res.json({
+      id,
+      title,
+      link,
+      description,
+      tags,
+    });
+  }
+}
+
+export default new RepositoryController();
